6124cdc6ffe1320814c31d007937652e54691d6d
SolarBear360 <121562307+SolarBear360@users.noreply.github.com>
2023-12-03 13:58:44 +0800

    Update UVA 10000 Longest Path.c

--- UVA 10000 Longest Path.c
+++ UVA 10000 Longest Path.c
@@ -8,39 +8,40 @@ struct numPair{
 struct numPair longest_path(int node[][100],int nodeNum,int start){
     static int count = 0;
     struct numPair longest_record;
-    struct numPair max_record;
     longest_record.x = 0;
     longest_record.y = 0;
-    max_record.x = 0;
-    max_record.y = 0;
-    int isterminal = 1;
+    struct numPair result;
+    result.x = count;
+    result.y = start;
     for(int i=0;i<nodeNum;i++){
-        if(node[start][i] == 0 && isterminal){
-            struct numPair result;
-            result.x = count;
-            result.y = start;
+        if(node[start][i] == 0){
             return result;
-        }else if(node[start][i] ==0 && !isterminal){
-
         }
-        isterminal = 0;
         count++;
         longest_record = longest_path(node,nodeNum,node[start][i]);
+        if(longest_record.x == result.x){
+            result = longest_record.y > result.y ? result : longest_record;
+        }else if(longest_record.x > result.x){
+            result = longest_record;
+        }
         count--;
-        printf("%d %d\n",longest_record.x,longest_record.y);
     }
+    return result;
+}
 
 int main(){
     int nodeNum;
-    while(scanf("%d",&nodeNum) != EOF){
-        int node[101][100];
-        for(int i=0;i<nodeNum;i++){
-            for(int j=0;j<nodeNum;j++){
-                node[i][j] = 0;
-            }
-        }
+    int caseCount = 0;
+    while(scanf("%d",&nodeNum)){
         if(nodeNum == 0){
             break;
+        }
+        caseCount++;
+        int node[101][100];
+        for(int i=0;i<=nodeNum;i++){
+            for(int j=0;j<100;j++){
+                node[i][j] = 0;
+            }
         }
         int start;
         scanf("%d",&start);
@@ -58,7 +59,7 @@ int main(){
             }
         }
         struct numPair answer = longest_path(node,nodeNum,start);
-        // printf("%d %d\n",answer.x,answer.y);
+        printf("Case %d: The longest path from %d has length %d, finishing at %d.\n",caseCount,start,answer.x,answer.y);
     }
     return 0;
 }
